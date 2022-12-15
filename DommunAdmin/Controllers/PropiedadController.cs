e33ad46ccd81c2756cc5feedb90030e5c8b9439a
Juan Carlos Reyes <ingreyesguerrero@gmail.com>
2022-12-15 14:01:06 -0500

    a
    a

--- DommunAdmin/Controllers/PropiedadController.cs
+++ DommunAdmin/Controllers/PropiedadController.cs
@@ -15,6 +15,12 @@ namespace DommunAdmin.Controllers
         private readonly IAgenteService agenteService;
         private readonly ITipoPropiedadService tipoPropiedadService;
         private readonly ITipoOfertaService tipoOfertaService;
+        private readonly IEstratoService estratoService;
+        private readonly ITiempoConstruidoService tiempoConstruidoService;
+        private readonly INumeroHabitacionService numeroHabitacionService;
+        private readonly INumeroBanoService numeroBanoService;
+        private readonly ITipoParqueaderoService tipoParqueaderoService;
+        private readonly ICaracteristicaParqueaderoService caracteristicaParqueaderoService;
 
         public PropiedadController(
             IPropiedadService _inmobiliariaService,
@@ -23,7 +29,13 @@ namespace DommunAdmin.Controllers
             ITipoPropiedadService _tipoPropiedadService,
             ICiudadService _ciudadService,
             IEstadoPropiedadService _estadoPropiedadService,
-            ITipoOfertaService _tipoOfertaService)
+            ITipoOfertaService _tipoOfertaService,
+            IEstratoService _estratoService,
+            ITiempoConstruidoService _tiempoConstruidoService,
+            INumeroHabitacionService numeroHabitacionService,
+            INumeroBanoService numeroBanoService,
+            ITipoParqueaderoService tipoParqueaderoService,
+            ICaracteristicaParqueaderoService caracteristicaParqueaderoService)
         {
             this.inmobiliariaService = _inmobiliariaService;
             this.commonServices = _commonServices;
@@ -32,6 +44,12 @@ namespace DommunAdmin.Controllers
             this.ciudadService = _ciudadService;
             this.estadoPropiedadService = _estadoPropiedadService;
             this.tipoOfertaService = _tipoOfertaService;
+            this.estratoService = _estratoService;
+            this.tiempoConstruidoService = _tiempoConstruidoService;
+            this.numeroHabitacionService = numeroHabitacionService;
+            this.numeroBanoService = numeroBanoService;
+            this.tipoParqueaderoService = tipoParqueaderoService;
+            this.caracteristicaParqueaderoService = caracteristicaParqueaderoService;
         }
 
         public async Task<ActionResult> Index()
@@ -61,39 +79,53 @@ namespace DommunAdmin.Controllers
         public async Task<ActionResult> CreateAsync()
         {
             List<SelectListItem> listAgente = new List<SelectListItem>();
-
             listAgente = await agenteService.GetSelectListItems();
-
             ViewBag.Agente = listAgente;
 
 
             List<SelectListItem> listTipoPropiedad = new List<SelectListItem>();
-
             listTipoPropiedad = await tipoPropiedadService.GetSelectListItems();
-
             ViewBag.TipoPropiedad = listTipoPropiedad;
 
 
             List<SelectListItem> listCiudad = new List<SelectListItem>();
-
             listCiudad = await ciudadService.GetSelectListItems();
-
             ViewBag.Ciudad = listCiudad;
 
 
             List<SelectListItem> listEstadoPropiedad = new List<SelectListItem>();
-
             listEstadoPropiedad = await estadoPropiedadService.GetSelectListItems();
-
             ViewBag.EstadoPropiedad = listEstadoPropiedad;
 
 
             List<SelectListItem> listTipoOferta = new List<SelectListItem>();
-
             listTipoOferta = await tipoOfertaService.GetSelectListItems();
-
             ViewBag.TipoOferta = listTipoOferta;
 
+
+            List<SelectListItem> listEstrato = new List<SelectListItem>();
+            listEstrato = await estratoService.GetSelectListItems();
+            ViewBag.Estrato = listEstrato;
+
+            List<SelectListItem> listTiempoConstruido = new List<SelectListItem>();
+            listTiempoConstruido = await tiempoConstruidoService.GetSelectListItems();
+            ViewBag.TiempoConstruido = listTiempoConstruido;
+
+            List<SelectListItem> listNumeroHabitacion = new List<SelectListItem>();
+            listNumeroHabitacion = await numeroHabitacionService.GetSelectListItems();
+            ViewBag.NumeroHabitacion = listNumeroHabitacion;
+
+            List<SelectListItem> listNumeroBano = new List<SelectListItem>();
+            listNumeroBano = await numeroBanoService.GetSelectListItems();
+            ViewBag.NumeroBano = listNumeroBano;
+
+            List<SelectListItem> listTipoParqueadero = new List<SelectListItem>();
+            listTipoParqueadero = await tipoParqueaderoService.GetSelectListItems();
+            ViewBag.TipoParqueadero = listTipoParqueadero;
+
+            List<SelectListItem> listCaracteristicaparqueadero = new List<SelectListItem>();
+            listCaracteristicaparqueadero = await caracteristicaParqueaderoService.GetSelectListItems();
+            ViewBag.Caracteristicaparqueadero = listCaracteristicaparqueadero;
 
             PropiedadDto model = new PropiedadDto();
 
